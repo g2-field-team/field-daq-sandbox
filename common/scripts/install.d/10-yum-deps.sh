fb816543e37a5431aba5261ebb61c1ebc908befc
Matthias W. Smith <mwsmith2@uw.edu>
2016-08-04 13:55:15 -0500

    Added install scripts for yum dependencies.

---
+++ common/scripts/install.d/10-yum-deps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -e "\e[31mInstalling requisite yum packages\e[0m"
+
+sudo yum install openssh-clients openssh-server libusb1-devel mysql-devel \
+    python-devel libpng-devel qt-devel PyQt4 tkinter cmake lapack blas \
+    libsodium-devel libXpm-devel devtoolset-2-gcc fftw-devel
+
+if [ "$(grep '/opt/rh/devtoolset-2/root/usr/bin' ~/.bashrc)" ]; then
+    echo -e "\e[31mdevtoolset already in PATH\e[0m"
+else
+    echo -e "\e[31mAdding devtoolset to PATH\e[0m"
+    echo -e "\nexport PATH=/opt/rh/devtoolset-2/root/usr/bin:\$PATH\n" >> ~/.bashrc
+fi
