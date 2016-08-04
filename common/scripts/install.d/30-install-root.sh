d9372735f6f3a194f4417d3b2cb27c46fc9c259a
Matthias W. Smith <mwsmith2@uw.edu>
2016-08-04 13:55:35 -0500

    Added install script to build ROOT.

---
+++ common/scripts/install.d/30-install-root.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e "\e[31m\nInstalling ROOT.\e[0m"
+
+mkdir -p ~/Packages && cd ~/Packages/
+
+if [ ! -d root ]; then
+    git clone http://root.cern.ch/git/root.git root
+fi
+
+cd root
+git checkout -b v5-34-08 v5-34-08
+./configure --all
+make
+
+if [ "$(grep 'thisroot.sh' ~/.bashrc)" ]; then
+    echo -e "\e[31mSource root line already in .bashrc\e[0m"
+else
+    echo -e "\e[31mAdding a line to source root to the .bashrc\e[0m"
+    echo -e "# Source ROOT setup script." >> ~/.bashrc
+    echo -e ". ~/Packages/root/bin/thisroot.sh\n" >> ~/.bashrc
+fi
