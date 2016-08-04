991b10e15fbda4ae8253cb78b88f92d826b0939b
Matthias W. Smith <mwsmith2@uw.edu>
2016-08-04 13:55:53 -0500

    Added install script to build MIDAS/ROME.

---
+++ common/scripts/install.d/40-gm2midas.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Note that you need an active kerberos ticket to run this.
+
+echo -e "\n\e[31mInstalling MIDAS/ROME\e[0m"
+
+if [ ! -e "~/.midas-env" ]; then
+    echo -e "\e[31mCopying MIDAS environment variables file.\e[0m"
+    cp ../midas-env ~/.midas-env
+fi
+
+if [ "$(grep midas-env '~/.bashrc')" ]; then
+    echo -e "\e[31mAdding line to .bashrc for MIDAS environment vars.\e[0m"
+    echo -e "# Source file with MIDAS environment variables." >> ~/.bashrc
+    echo -e ". ~/.midas-env" >> ~/.bashrc
+fi
+
+cd ~
+. .midas-env
+
+mkdir -p ~/Packages && cd ~/Packages
+
+if [ ! -d "gm2midas" ]; then
+    git clone ssh://p-gm2midas@cdcvs.fnal.gov/cvs/projects/gm2midas
+fi
+
+cd gm2midas
+make -C midas
+make -C mscb
+make -C rome
+
+if [ ! -d "g2-midas" ]; then
+    echo -e "\e[31mUpdating MIDAS to a newer version.\e[0m"
+    mv midas g2-midas
+
+    git clone https://bitbucket.org/tmidas/midas.git
+    cd midas
+    git checkout e885e8f
+    make
+fi
+
