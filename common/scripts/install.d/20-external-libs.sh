e9c2e0e0fc8083944cd4d1dbfd7606b8b54708e3
Matthias W. Smith <mwsmith2@uw.edu>
2016-08-04 15:36:14 -0500

    Add install script for external libs (like boost).

---
+++ common/scripts/install.d/20-external-libs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -e "\e[31mInstalling requisite external packages\e[0m"
+
+# Install boost.
+if [ -d ~/Packages/boost ]; then
+    echo -e "\e[31mBoost is already installed.\e[0m"
+else
+    echo -e "\e[31mInstalling boost.\e[0m"
+    cd ~/Packages
+    git clone --recursive https://github.com/boostorg/boost
+
+    cd boost
+    ./bootstrap.sh
+    sudo ./b2 install
+fi
