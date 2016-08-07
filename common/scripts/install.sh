9e9886cd3074a3beab236f2bac20cfc384de7575
Matthias W. Smith <mwsmith2@uw.edu>
2016-08-07 01:56:06 -0500

    Splitting install scripts into sudo/non-sudo.

--- common/scripts/install.sh
+++ common/scripts/install.sh
@@ -4,7 +4,13 @@
 INSTALL_DIR="$(dirname $(readlink -f $0))/install.d"
 cd $INSTALL_DIR
 
-for script in `ls .`
+for script in `ls [0-4]?-*.sh`
+do
+    sudo . $script
+    cd $INSTALL_DIR
+done
+
+for script in `ls [5-9]?-*.sh`
 do
     . $script
     cd $INSTALL_DIR
