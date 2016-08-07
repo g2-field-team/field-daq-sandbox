476b260210085a7565754decfabd6e0ccd539b7d
Matthias W. Smith <mwsmith2@uw.edu>
2016-08-07 01:07:00 -0500

    Add flag to auto agree to install.

--- common/scripts/install.d/10-yum-deps.sh
+++ common/scripts/install.d/10-yum-deps.sh
@@ -2,7 +2,7 @@
 
 echo -e "\e[31mInstalling requisite yum packages\e[0m"
 
-yum install openssh-clients openssh-server libusb1-devel mysql-devel \
+yum -y install openssh-clients openssh-server libusb1-devel mysql-devel \
     python-devel libpng-devel qt-devel PyQt4 tkinter cmake lapack blas \
     libsodium-devel libXpm-devel devtoolset-2-gcc devtoolset-2-gcc-c++ \
     fftw-devel unixODBC
