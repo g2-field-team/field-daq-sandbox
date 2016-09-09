530a162611fbfb64b6a7786ac91894ed49da0fd6
Matthias W. Smith <mwsmith2@uw.edu>
2016-09-09 15:41:58 -0500

    Adding scripts to initialize odb parameters.

---
+++ common/examples/common/odb.d/20-simple-fe.cmd
@@ -0,0 +1,7 @@
+mkdir "Equipment/Simple Frontend/Settings"
+
+create DOUBLE "Equipment/Simple Frontend/Settings/mean"
+set "Equipment/Simple Frontend/Settings/mean" 0.0
+
+create DOUBLE "Equipment/Simple Frontend/Settings/sigma"
+set "Equipment/Simple Frontend/Settings/sigma" 1.0
