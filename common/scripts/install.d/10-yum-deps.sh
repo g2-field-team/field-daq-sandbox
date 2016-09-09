8e6cc2cfd0b4a530b9e04e458480bec49a394f35
Matthias W. Smith <mwsmith2@uw.edu>
2016-09-08 20:58:29 -0500

    Adds sqlite-devel to yum install packages.

--- common/scripts/install.d/10-yum-deps.sh
+++ common/scripts/install.d/10-yum-deps.sh
@@ -5,4 +5,4 @@ echo -e "\e[31mInstalling requisite yum packages\e[0m"
 yum -y install openssh-clients openssh-server libusb1-devel mysql-devel \
     python-devel libpng-devel qt-devel PyQt4 tkinter cmake lapack blas \
     libsodium-devel libXpm-devel devtoolset-2-gcc devtoolset-2-gcc-c++ \
-    devtoolset-2-binutils-devel fftw-devel unixODBC
+    devtoolset-2-binutils-devel fftw-devel unixODBC sqlite-devel
