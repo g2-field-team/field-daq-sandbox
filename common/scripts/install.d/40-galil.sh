99950ddf108ae081c4860ddaa35badc6c1c047f5
hongran <hongran@uw.edu>
2016-09-13 00:35:35 -0500

    modify galil

--- common/scripts/install.d/40-galil.sh
+++ common/scripts/install.d/40-galil.sh
@@ -1,11 +1,11 @@
-sudo mkdir -p /usr/local/opt/galil
+mkdir -p /usr/local/opt/galil
 cd /usr/local/opt/galil
-sudo wget http://www.galil.com/sw/pub/rhel/6/galilrpm-2-1.noarch.rpm | sudo rpm -ivh
+wget http://www.galil.com/sw/pub/rhel/6/galilrpm-2-1.noarch.rpm | rpm -ivh
 
-sudo yum install gclib
+yum install gclib
 
-sudo wget http://www.galilmc.com/download/software/galilsuite/linux/galil_public_key.asc
-su -c 'rpm --import galil_public_key.asc'
+wget http://www.galilmc.com/download/software/galilsuite/linux/galil_public_key.asc
+rpm --import galil_public_key.asc
 
 sudo wget http://www.galilmc.com/download/software/galilsuite/linux/galilsuite.x86_64.rpm
-su -c 'rpm -i galilsuite.x86_64.rpm'
+rpm -i galilsuite.x86_64.rpm
